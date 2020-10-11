e1852041fe90effb0e79607d807f3066c59af7e5
rajal841 <70993372+rajal841@users.noreply.github.com>
2020-10-11 17:53:53 -0400

    Update setup.php

--- inc/setup.php
+++ inc/setup.php
@@ -2,13 +2,13 @@
 
 session_start();
 
-require_once( $_SERVER['DOCUMENT_ROOT'] . '/example-woo-api-app-master/vendor/autoload.php' );
-require_once( $_SERVER['DOCUMENT_ROOT'] . '/example-woo-api-app-master/inc/functions.php' );
-require_once( $_SERVER['DOCUMENT_ROOT'] . '/example-woo-api-app-master/inc/form.php' );
-require_once( $_SERVER['DOCUMENT_ROOT'] . '/example-woo-api-app-master/inc/database.php' );
-require_once( $_SERVER['DOCUMENT_ROOT'] . '/example-woo-api-app-master/inc/user.php' );
-require_once( $_SERVER['DOCUMENT_ROOT'] . '/example-woo-api-app-master/inc/woocommerce.php' );
-require_once( $_SERVER['DOCUMENT_ROOT'] . '/example-woo-api-app-master/inc/actions.php' );
-require_once( $_SERVER['DOCUMENT_ROOT'] . '/example-woo-api-app-master/inc/notices.php' );
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/woocommerce_rest_api/vendor/autoload.php' );
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/woocommerce_rest_api/inc/functions.php' );
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/woocommerce_rest_api/inc/form.php' );
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/woocommerce_rest_api/inc/database.php' );
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/woocommerce_rest_api/inc/user.php' );
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/woocommerce_rest_api/inc/woocommerce.php' );
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/woocommerce_rest_api/inc/actions.php' );
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/woocommerce_rest_api/inc/notices.php' );
 
 iconic_process_actions();
